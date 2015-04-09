4dd3d7b0d5a725af08b83ad8a670cca885d311e9
Shao Fei <sfatchs@yahoo.com.sg>
2015-04-09 14:29:24 +0800

    created junit test classes

---
+++ quicklyst/src/test/FieldParserTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FieldParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetField() {
+		fail("Not yet implemented");
+	}
+
+}
